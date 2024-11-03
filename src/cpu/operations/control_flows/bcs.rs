dfba2bd69bb15a4457da05461fae4a764cf31757
Pinyoo Thotaboot <pinyoo_too@hotmail.com>
2024-11-03 14:21:39 +0700

    Create BCS

--- src/cpu/operations/control_flows/bcs.rs
+++ src/cpu/operations/control_flows/bcs.rs
@@ -2,34 +2,36 @@ use crate::cpu::model::{State, CPU};
 
 impl CPU {
     /**
-     * BCC - Branch on Carry Clear
-        Operation: Branch on C = 0
+     * BCS - Branch on Carry Set
+        Operation: Branch on C = 1
 
-        This instruction tests the state of the carry bit and takes a conditional branch if the carry bit is reset.
+        This instruction takes the conditional branch if the carry flag is on.
 
-        It affects no flags or registers other than the program counter and then only if the C flag is not on.
+        BCS does not affect any of the flags or registers except for the program counter and only then if the carry flag is on.
 
         Addressing Mode	Assembly Language Form	Opcode	No. Bytes	No. Cycles
-        Relative	            BCC $nnnn	     $90	    2	        2+t+p
+        Relative	            BCS $nnnn	      $B0	    2	        2+t+p
         p: =1 if page is crossed.
         t: =1 if branch is taken.
      */
-    pub fn bcc(&mut self) {
-        self.bcc_relative();
-        self.bcc_run();
+    pub fn bcs(&mut self) {
+        self.bcs_relative();
+        self.bcs_run();
     }
 
-    fn bcc_relative(&mut self) {
+    fn bcs_relative(&mut self) {
         self.pc +=1;
         self.address = self.pc.clone();
         // Fetch Branch Offset
         self.data = self.read(&self.address);
     }
 
-    fn bcc_run(&mut self) {
-        if self.status.get_carry() == 0 {
+    fn bcs_run(&mut self) {
+        if self.status.get_carry() == 1 {
             let offset = self.data;
+
             // TODO :: Tick
+
             self.address_register = self.pc as u16 & 0x00FF + offset as u16 & 0x00FF + 0x0001;
             self.address = self.address_register;
 
@@ -39,6 +41,7 @@ impl CPU {
 
             // Offset Added to Program Counter
             self.pc = self.address;
+
         }
     }
 }
